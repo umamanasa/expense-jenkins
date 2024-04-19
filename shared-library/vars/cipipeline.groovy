5a1467a0101165bc6377d566e347d6592e01826d
umamanasa <umamanasa49@gmail.com>
2024-04-19 18:41:12 +0530

    cipipeline.groovy

---
+++ shared-library/vars/cipipeline.groovy
@@ -0,0 +1,34 @@
+def call() {
+
+  node('workstation') {
+
+    sh "find . | sed -e '1d' |xargs rm -rf "
+
+    if(env.TAG_NAME ==~ ".*"){
+      env.branch_name = "refs/tags/${env.TAG_NAME}"
+    } else {
+      env.branch_name = "${env.BRANCH_NAME}"
+    }
+    checkout scmGit(
+        branches: [[name: branch_name]],
+        userremoteconfigs: [[url: "https://github.com/umamanasa/${component}"]]
+    )
+
+    if(env.TAG_NAME ==~ ".*") {
+      common.compile()
+      common.release()
+    } else {
+      if(env.BRANCH_NAME == "main") {
+        common.compile()
+        common.test()
+        common.codeQuality()
+        common.codeSecurity()
+      } else {
+        common.compile()
+        common.test()
+        common.codeQuality
+      }
+    }
+  }
+}
+
---
+++ shared-library/vars/common.groovy
@@ -0,0 +1,36 @@
+def compile() {
+  if (env.codeType == "static") {
+    return "Don't need any Compilation"
+  }
+
+  stage('Compile Code') {
+    if(env.codeType == "nodejs"){
+      sh 'npm install'
+    }
+  }
+
+}
+
+def test() {
+  stage('Test Cases') {
+    print 'Test'
+  }
+}
+
+def codeQuality() {
+  stage('Code Quality') {
+    print 'Code Quality'
+  }
+}
+
+def codeSecurity() {
+  stage('Code Security') {
+    print 'Code Security'
+  }
+}
+
+def release() {
+  stage('Release') {
+    print 'Release'
+  }
+}
