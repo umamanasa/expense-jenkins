8296fc23ec33743e4d91bee91f8d19f0ed07687b
umamanasa <umamanasa49@gmail.com>
2024-04-22 13:16:11 +0530

    cipipeline.groovy

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -21,7 +21,9 @@ def test() {
 
 def codeQuality() {
   stage('Code Quality') {
-    print 'Code Quality'
+    sonaruser = sh(script: 'aws ssm get-parameter --name "sonarqube.user" --with-decryption --query="Parameter.Value"', returnStatus: true)
+    sonarpass = sh(script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value"', returnStatus: true)
+    sh 'sonar-scanner -Dsonar.host.url=http://172.31.25.122:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
   }
 }
 
