d3a436700e7e40f7db00550f652d4f4157f95e5e
umamanasa <umamanasa49@gmail.com>
2024-04-22 15:11:27 +0530

    code quality

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -22,8 +22,8 @@ def test() {
 
 def codeQuality() {
   stage('Code Quality') {
-    env.sonaruser = sh(script: 'aws ssm get-parameter --name "sonarqube.user" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-    env.sonarpass = sh(script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+    env.sonaruser = sh (script: 'aws ssm get-parameter --name "sonarqube.user" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+    env.sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
     wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
       sh 'sonar-scanner -Dsonar.host.url=http://172.31.25.122:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
     }
